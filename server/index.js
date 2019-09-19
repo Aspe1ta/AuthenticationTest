5a884d5fc72adc5ae4def9e6aa3709bd84add121
Austin Irvine <ausy11@live.ca>
2019-09-19 10:24:38 -0400

    styleing

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+let logininfo = [
+    {
+        user: 'Austin',
+        password: 'test1'
+    }
+    ,
+    {
+        user: 'Rhi',
+        password: 'test2'
+    }
+]
+
+
+
